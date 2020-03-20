282a25063fa18cd075b0abeff8b8ecb8af2ebc6e
Dídac García <didaquis@gmail.com>
2020-03-20 20:59:52 +0100

    Code has been refactored

--- src/utils/router-helpers.js
+++ src/utils/router-helpers.js
@@ -4,21 +4,21 @@
  * Generate a response object (successful cases)
  * @param {string} [data] - Data to send in response 
  */
-function success (data) {
+const success = (data) => {
 	const res = { status: 'OK' };
 	if (data) res.data = data;
 	return res;
-}
+};
 
 /**
  * Generate a response object (failed cases)
  * @param {string} [error] - Error information to send in response 
  */
-function fail (error) {
+const fail = (error) => {
 	const res = { status: 'ERROR' };
 	if (error) res.error = error;
 	return res;
-}
+};
 
 /**
  * @constant
